ae64f548918745d4392021a1827f60097ddf263d
kelli <kwisuri@gmail.com>
2016-07-02 20:30:13 -0700

    Cleaned up code: removed views that did not utilize Django generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,11 +8,6 @@ from .models import Question, Choice
 
 
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -22,22 +17,17 @@ class IndexView(generic.ListView):
         return Question.objects.order_by('-pub_date')[:5]
 
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
 
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    
 
 
 def vote(request, question_id):
